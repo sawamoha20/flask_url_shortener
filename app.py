788932dbe2628bfc24e836773de2e3c7922aae66
sawamoha20 <sawamoha20@hmail.com>
2020-11-14 15:05:12 +0100

    added delete button

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import string
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///URLS.db'
+db = SQLAlchemy(app)
 
 def get_random_string(length):
     letters = string.ascii_lowercase
@@ -12,20 +13,36 @@ def get_random_string(length):
     return result_str
 
 
-class URLS:
+class URLS(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    URl = db.Column(db.Text, nullable=False)
-    shrt_url = db.Column(db.Text,nullable=False)
-    def __repr__(self):
-        return 'url ' + str(self.id)
+    URl = db.Column(db.String(200), nullable=False)
+    shrt_value = db.Column(db.String(200),nullable=False,unique=True)
+    
+    def __init__(self,URl,shrt_value):
+        self.URl = URl
+        self.shrt_value = shrt_value
 
 
-@app.route('/', methods = ['POST , GET'])
-def home():
+@app.route('/',methods=['POST','GET'])
+def index():
     if request.method == 'POST':
-        link = request.form['main_url']
-        short_url = get_random_string(5)
-        
+        URl = request.form['main-url']
+        shrt_value = get_random_string(5)
+        link = URLS(URl, shrt_value)
+        db.session.add(link)
+        db.session.commit()
+    Data = URLS.query.all()
+    print(Data)
+    return render_template('index.html',Data = Data)
+
+@app.route('/<value>')       
+def unshorten(value):
+    dblink = db.session.query(URLS).filter(URLS.shrt_value==value).first()
+    print(dblink)
+    link = dblink.URl
+    return redirect('/')
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
