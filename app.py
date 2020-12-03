6e08850efeabbd2f1a3909534b3fb2e68523190b
sawamoha20 <sawamoha20@hmail.com>
2020-12-03 19:44:32 +0100

    .

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, jsonify
+from flask import Flask, render_template, request, redirect, flash, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
 import urllib.parse
@@ -85,10 +85,11 @@ def api_delete(id):
     link = URLS.query.get_or_404(id)
     db.session.delete(link)
     db.session.commit()
-    return redirect(url_for('index'))
+    return 'deleted' ,202
+
 if __name__ == "__main__":
     app.secret_key = 'SLrs5ThKt9z'
-    app.run(host='0.0.0.0',debug = True)
+    app.run(debug = True)
 
 
 
